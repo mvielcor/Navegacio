0a23be41ba74117f89367f12331a57a733d0192d
Manel <mviel@florida-uni.es>
2021-01-21 14:56:28 +0100

    Navegació Web 3 pantalles

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import {
   StyleSheet,
@@ -16,19 +17,36 @@ import {
   StatusBar,
 } from 'react-native';
 
-import {Button} from 'react-native-elements'
+import { Button } from 'react-native-elements';
+import { Pantalla1} from './screens/Pantalla1';
+import { Pantalla2} from './screens/Pantalla2';
+import { Pantalla3} from './screens/Pantalla3';
+
+const pilaNavegacio = createStackNavigator(); //Creem la pila de Navegació
 
 export class App extends Component {
+  
+pantallaPrincipal = ({navigation}) => {return(
+  <View style={styles.contenidor}>
+  <StatusBar barStyle="dark-content" />
+  <View>
+    <Text> Pantalla inicial </Text>
+    <Button title='Pantalla 1' onPress={()=>navigation.navigate('Pantalla 1')} /> 
+    <Button title='Pantalla 2' onPress={()=>navigation.navigate('Pantalla 2')} /> 
+  </View>
+</View>
+)}
+
+
   render() {
     return (
       <NavigationContainer>
-        <View style={styles.contenidor}>
-          <StatusBar barStyle="dark-content" />
-          <View >
-              <Text> Pantalla inicial </Text>
-              
-          </View>
-        </View>
+        <pilaNavegacio.Navigator>
+          <pilaNavegacio.Screen name="Home" component={this.pantallaPrincipal} />
+          <pilaNavegacio.Screen name="Pantalla 1" component={Pantalla1} />
+          <pilaNavegacio.Screen name="Pantalla 2" component={Pantalla2} />
+          <pilaNavegacio.Screen name="Pantalla 3" component={Pantalla3} />
+        </pilaNavegacio.Navigator>
       </NavigationContainer>
     );
   }
@@ -36,16 +54,16 @@ export class App extends Component {
 
 const styles = StyleSheet.create({
   contenidor: {
-    flex:1,
-    flexDirection:'column',
-    justifyContent:'center',
+    flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
-    padding:5,
-    margin:5,
+    justifyContent: 'center',
+    padding: 5,
+    margin: 5,
     alignContent: 'space-around',
-    alignItems:'center',
+    alignItems: 'center',
   },
-  
+
 });
 
 export default App;
